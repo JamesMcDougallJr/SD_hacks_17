e306ad5a432f2a1d6b734e34a64d4e35a0e3dc8d
anh190 <30811820+anh190@users.noreply.github.com>
2017-10-21 23:36:26 -0700

    Update main.java

--- Traffic Safety/src/main.java
+++ Traffic Safety/src/main.java
@@ -18,46 +18,45 @@ private static String locationToken;
 		Event e = gson.fromJson(dr.getPedestrianData(0), Event.class);
 		locations.add(new Node(e.getContent().get(0).getLocationUid(),
                             e.getContent().get(0).getMeasures().getCarSpeed(),
-<<<<<<< HEAD
+
                             e.getContent().get(0).getMeasures().getVehicleCount(),e.getContent().get(0).getMeasures().getNumPeds()));
-=======
-                            e.getContent().get(0).getMeasures().getVehicleCount(),
-														e.getContent().get(0).getPedestrian().getPedestrianCount());
->>>>>>> f5c42b73975f5a319d1bf7b74fb5392c9f9039a8
-    locationToken=e.getContent.get(0).getLocationUid();
-    j=e.getContent().size();
+
+                            //e.getContent().get(0).getMeasures().getVehicleCount(),
+			    //e.getContent().get(0).getPedestrian().getPedestrianCount());
+
+    		locationToken=e.getContent.get(0).getLocationUid();
+    		j=e.getContent().size();
 		for (int i = 1; i < j; i++)	{
 /**
  * decide whether the locations has a node with this value
- */   int start = i;
-      int end = j;
-      while( start - 1 <= end - 1) {
-        int middle = (start + end) / 2;
-        if (locationToken == locations.get(middle)) {
-          Node.append(locationToken, data, data, data);
-        }
-        if(locationToken < locations.get(middle)) {
-          end = middle - 1;
-        }
-        else {
-          start = middle + 1;
-        }
-      } 
-      if(locations.contains(locationToken)=false) {
-        locations.add(new
-                        Node(e.getContent().get(i).getLocationUid(),e.getContent().get(i).getMeasures().getCarSpeed(),
+ */   		int start = i;
+      		int end = j;
+      		while( start - 1 <= end - 1) {
+       		 int middle = (start + end) / 2;
+        	if (locationToken == locations.get(middle)) {
+          	  Node.append(locationToken, data, data, data);
+        	}
+        	if(locationToken < locations.get(middle)) {
+          	  end = middle - 1;
+        	}
+        	else {
+          	  start = middle + 1;
+        	}
+      		} 
+      		if(locations.contains(locationToken)=false) {
+        	  locations.add(new Node(e.getContent().get(i).getLocationUid(),e.getContent().get(i).getMeasures().getCarSpeed(),
                                 e.getContent().get(i).getMeasures().getVehicleCount(),
                                 e.getContent().get(i).getMeasures().getNumPeds()));
                                 
-      }
+      		}
 			e.getContent().get(i);
 		}
 		
 		System.out.println(e);
 		
-	}
+		}
 
-	ArrayList <Node> sortDangerLevel()	{ 
+		ArrayList <Node> sortDangerLevel()	{ 
 		//insert sorting algorithm by descending order
 
 			 if (locations.get(locations.size()/2) !=  locationToken ) {
